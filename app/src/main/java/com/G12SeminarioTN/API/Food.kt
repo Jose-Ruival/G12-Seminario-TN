f3bfd377cee438d12a7d4c51087c7ad621d61d02
Jose-Ruival <josemanuelruival@gmail.com>
2024-10-18 15:13:55 -0300

    Api atributos parte 1

---
+++ app/src/main/java/com/G12SeminarioTN/API/Food.kt
@@ -0,0 +1,6 @@
+package com.G12SeminarioTN.API
+
+class Food (
+    val uri: String,
+    val label: String
+)
---
+++ app/src/main/java/com/G12SeminarioTN/API/Ingrediente.kt
@@ -0,0 +1,12 @@
+package com.G12SeminarioTN.API
+
+
+
+class Ingrediente (
+    val uri: String,
+    val quantity: Float,
+    //val measure: Measure//,
+    val weight: Float,
+    //val food: Food//
+)
+

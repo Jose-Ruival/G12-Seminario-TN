018cb7fba973dd8a0b697c2949b0b9ae2235bab4
Jose-Ruival <josemanuelruival@gmail.com>
2024-10-18 16:09:41 -0300

    Api implementacion 1

--- app/src/main/java/com/G12SeminarioTN/API/RetroFitClient.kt
+++ app/src/main/java/com/G12SeminarioTN/API/RetroFitClient.kt
@@ -9,12 +9,17 @@ object RetroFitClient {
 
     private val BASE_URL = "https://api.edamam.com"
 
+    // Crear la instancia de Moshi
     private val moshi = Moshi.Builder()
-        .addLast(KotlinJsonAdapterFactory())
+        .add(KotlinJsonAdapterFactory()) // Agregar soporte para Kotlin
         .build()
 
-    val retroFit = Retrofit.Builder()
+    // Crear la instancia de Retrofit
+    val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .addConverterFactory(MoshiConverterFactory.create(moshi)) // Usar Moshi como convertidor
         .build()
+
+    // Crear el servicio API
+    val apiService: PostEndpoints = retrofit.create(PostEndpoints::class.java)
 }

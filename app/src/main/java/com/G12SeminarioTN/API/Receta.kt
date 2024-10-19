d305217507e6d07f8fc0fd7c1c4a5a1a7dc06b5c
Jose-Ruival <josemanuelruival@gmail.com>
2024-10-19 10:19:41 -0300

    Implementacion api 4

--- app/src/main/java/com/G12SeminarioTN/API/Receta.kt
+++ app/src/main/java/com/G12SeminarioTN/API/Receta.kt
@@ -1,19 +1,18 @@
 package com.G12SeminarioTN.API
 
 data class Receta(
-    val uri: String,                         // Identificador de ontología
-    val label: String,                       // Título de la receta
-    val image: String,                       // URL de la imagen
-    val source: String,                      // Identificador de sitio de origen
-    val url: String,                         // URL de la receta original
-    val yield: Int,                          // Porciones que rinde
-    val summary: String?,                    // Breve descripción
-    val calories: Float?,                    // Total de energía, kcal
-    val totalWeight: Float?,                 // Peso total, g
-    val ingredients: List<Ingrediente>,       // Lista de ingredientes
-    val totalNutrients: List<NutrienteInfo>?, // Nutrientes totales por porción
-    val totalDaily: List<NutrienteInfo>?,    // % del valor diario por porción
-    val dietLabels: List<String>?,           // Etiquetas de dieta
-    val healthLabels: List<String>?          // Etiquetas de salud
-
+    val uri: String,
+    val label: String,
+    val image: String,
+    val source: String,
+    val url: String,
+    val yield: Int,
+    val summary: String?,
+    val calories: Float?,
+    val totalWeight: Float?,
+    val ingredients: List<Ingrediente>,
+    val totalNutrients: List<NutrienteInfo>?,
+    val totalDaily: List<NutrienteInfo>?,
+    val dietLabels: List<String>?,
+    val healthLabels: List<String>?
 )

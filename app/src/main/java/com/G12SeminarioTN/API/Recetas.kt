fd4c7c9977f675710a4a441ba4e9f46b5e2afb41
Jose-Ruival <josemanuelruival@gmail.com>
2024-10-20 22:55:27 -0300

    Implementacion api 6

--- app/src/main/java/com/G12SeminarioTN/API/Recetas.kt
+++ app/src/main/java/com/G12SeminarioTN/API/Recetas.kt
@@ -3,6 +3,6 @@ package com.G12SeminarioTN.API
 import com.squareup.moshi.Json
 
 data class Recetas(
-    @Json(name = "recipes")
-    val recipes: Array<Receta>
+    @Json(name = "hits")
+    val hits: List<Hit>
 )

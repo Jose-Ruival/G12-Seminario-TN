a876ff444ed8b25bcf480d5bc02167c3f5f19c23
muriRam <murielramello@gmail.com>
2024-10-20 21:41:58 -0300

    agrego atributos Receta

--- app/src/main/java/com/G12SeminarioTN/API/Receta.kt
+++ app/src/main/java/com/G12SeminarioTN/API/Receta.kt
@@ -5,11 +5,23 @@ import com.squareup.moshi.Json
 data class Receta(
     @Json(name = "name")
     val name: String,
+    @Json (name = "ingredients")
+    val ingredients: Array<String>,
+    @Json (name = "instructions")
+    val instructions: Array<String>,
+    @Json (name = "prepTimeMinutes")
+    val prepTimeMinutes: Int,
+    @Json (name = "cookTimeMinutes")
+    val cookTimeMinutes: Int,
     @Json(name = "cuisine")
     val cuisine: String,
-    @Json(name = "difficulty")
-    val difficulty: String
-)
+    @Json (name = "caloriesPerServing")
+    val caloriesPerServing: Int,
+    @Json (name = "image")
+    val image: String,
+    @Json (name = "mealType")
+    val mealType: String
+    )
 
 
 
